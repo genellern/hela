469097225cbaa77f492977c37e7c60687324b77b
Geneller Naranjo <4094671+genellern@users.noreply.github.com>
2025-06-11 18:28:12 -0500

    Add utilitary functions

---
+++ cmd/utils/slices.go
@@ -0,0 +1,12 @@
+package utils
+
+func Filter[T any](slice []T, fn func(T) bool) []T {
+    var result []T
+    for _, v := range slice {
+        if fn(v) {
+            result = append(result, v)
+        }
+    }
+
+    return result
+}

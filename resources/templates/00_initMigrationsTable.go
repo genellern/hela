ec49b4238fc3d3dcc9408141d024b5febe9969bd
Geneller Naranjo <4094671+genellern@users.noreply.github.com>
2025-06-18 17:30:45 -0500

    Add missing values to initial migration template

--- resources/templates/00_initMigrationsTable.go
+++ resources/templates/00_initMigrationsTable.go
@@ -5,11 +5,12 @@ import "github.com/genellern/hela/cmd/migrator"
 func (m *Migration) Migration_0000_InitMigrationsTable() migrator.Migration {
     return migrator.Migration{
         Version:     0,
+        Name:        "Init migrations",
         Table:       "migrations",
         PackageName: "migrations",
         Action:      migrator.Create,
         Fields: []string{
-            "migration_name:string",
+            "migration_name:varchar(120)",
             "migration_version:int(5)",
             "migrated_on:datetime",
         },
